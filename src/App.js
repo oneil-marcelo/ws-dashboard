e374547c931c50b20a5c2f35d42ae2f9b9b86ce2
Marcelo Henrique dos Santos <oneil@Marcelos-MacBook-Air.local>
2020-03-21 20:06:13 -0300

    adicionando sistema de rota instalando pacote react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './components/GlobalStyle';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import Routes from './components/Routes';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Menu />
+      <Routes />
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Main from '../../pages/Main';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Main} />
+    </Switch>
+  );
+};
+
+export default Routes;
